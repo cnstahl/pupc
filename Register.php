050b7ee178f567059448761f8716e8716cbc986f
Ming-Yee Tsang <mingyee@gmx.net>
2016-10-05 20:04:01 -0400

    Updated home page, changed registration/account creation workflow.

--- Register.php
+++ Register.php
@@ -7,25 +7,50 @@ $year = 2016; // Be sure to make the database table before updating this value
 
 // Set registration variables
 $submit = $_POST["submit"];
+
+$password = $_POST["password"];
+$repassword = $_POST["repassword"];
+$email = $_POST["email"];
+$username = $_POST["username"];
+
 $format = $_POST["format"];
 $type = $_POST["type"];
 $aid = $_POST["aid"];
 $note = $_POST["note"];
 
-// Must be authenticated to register
-if (!logged_in() || !verified())
-	print $renderer->render('Sorry.html.twig', array());
-else
-{
-	$uid = safe($_COOKIE["Plink_uid"], 'sql');
+if ($password) {
+	$email = safe($email, "sql");
+	$username = safe($username, "sql");
+	
 	// Verify that the registration form was submitted and that the email & password are correct
-	if ($submit)
-		if (register_PUPC($uid, $format, $type, $aid, $note, $year))
-			create_alert("Registered successfully!", "success");
-		else
-			create_alert("There was a problem with your registration. You may already be registered.", "danger");
-		
+	if (valid_registration($password, $repassword, $email) && register($password, $email))
+		$register_result = create_alert("Registered successfully! Verification email sent to $email.", "info");
+	else
+		$register_result = create_alert(registration_error($password, $repassword, $email), "danger");
+	
 	// Load page
-	print $renderer->render('Register.html.twig', array());
+	if ($register_result == "success")
+		header('Location: index.php');
+	else
+		print $renderer->render('MakeAccount.php.twig', array());
+}
+else {
+	// Must be authenticated to register for PUPC
+	if (!logged_in() || !verified()) {
+		create_alert("Please create an account or log in before registering.", "warning");
+		print $renderer->render('MakeAccount.html.twig', array());
+	}
+	else {
+		$uid = safe($_COOKIE["Plink_uid"], 'sql');
+		// Verify that the registration form was submitted and that the email & password are correct
+		if ($submit)
+			if (register_PUPC($uid, $format, $type, $aid, $note, $year))
+				create_alert("Registered successfully!", "success");
+			else
+				create_alert("There was a problem with your registration. You may already be registered.", "danger");
+			
+		// Load page
+		print $renderer->render('Register.html.twig', array());
+	}
 }
 ?>
